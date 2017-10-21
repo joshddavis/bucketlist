d9b6c42498242e4ed509cf7c21353187016ab70e
Josh <josh.oasis@gmail.com>
2017-10-21 04:17:20 +0000

    built 2 functions. first function populates local storage with shopping list. second populates list via local storage.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,28 @@ var shoppingList = [
   {dept: 'Produce', items: []},
 ];
 
+//function to populate local storage with shopping list
+function updateStorage() {
+  shoppingList.forEach(function (element) {
+    if (element.items.length > 0) {
+      window.localStorage.setItem(element.dept, JSON.stringify(element.items));
+    }
+  });
+}
+
+//function to populate shopping list with local storage
+function getStorage() {
+  for (var i=0; i < window.localStorage.length; i++) {
+    shoppingList.forEach(function (element) {
+      if (element.dept === window.localStorage.key(i)) {
+        var key = window.localStorage.key(i);
+        element.items = JSON.parse(window.localStorage.getItem(key));
+      }
+    });
+  }  
+  console.log(shoppingList);
+}
+
 //function to build list of items using DOM node functions
 function buildList () {
   var list = document.getElementById('list');
@@ -50,6 +72,9 @@ function encodeItem(x) {
 //start by hiding the list element then focus on input field
 document.getElementById('list').style.display = 'none';
 document.getElementById('item').focus();
+getStorage();
+
+
 
 //populate array with entered item then write list to page
 form.addEventListener('submit', function(e) {
@@ -58,11 +83,12 @@ form.addEventListener('submit', function(e) {
   var department = document.getElementById('department').value;
   var position = shoppingList.findIndex(i => i.dept.toLowerCase() === department.toLowerCase());
   shoppingList[position].items.push(item);                    
-  buildList ();
+  buildList();
   document.querySelector('.dept').style.cssText = 'border-radius: 5px 5px 0 0; margin-top: 10px;';
   document.getElementById('list').style.display = 'block';
   document.getElementById('item').value = '';
   document.getElementById('item').focus();
+  updateStorage();
 });
 
 //sets focus back on the item field after changing department
@@ -76,7 +102,7 @@ document.getElementById('list').addEventListener('click', function(e) {
 	var parent = e.target.parentNode;
 	var depart = parent.previousSibling;
 	var itemToDelete = encodeItem(e.target.innerText);
-	for (i=0; i < shoppingList.length; i++) {
+	for (var i=0; i < shoppingList.length; i++) {
 		if ( depart.innerText == shoppingList[i].dept ) {
 				var deletePos = shoppingList[i].items.findIndex(x => x.items === itemToDelete);
 				shoppingList[i].items.splice(deletePos, 1);
