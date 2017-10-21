9e856e4c3fd88c946eea3dea25e6388e1d004520
Josh <josh.oasis@gmail.com>
2017-10-21 17:49:47 +0000

    Added initialLoad function to build and show list at page load if local storage contained items. also updated index.html to point to itself regarding the form action

--- js/scripts.js
+++ js/scripts.js
@@ -66,17 +66,28 @@ function buildList () {
 
 //function to HTML encode user input
 function encodeItem(x) {
-	return x.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;");
+	return x.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").trim();
 }
 
-//start by hiding the list element then focus on input field
-document.getElementById('list').style.display = 'none';
-document.getElementById('item').focus();
-getStorage();
+//function for initial page load
+function initialLoad() {
+  document.getElementById('list').style.display = 'none';
+  getStorage();
+  buildList();
+  if ( document.getElementById('list').innerText.trim() === '' ) {
+    document.getElementById('list').style.display = 'none';
+  } else { 
+    document.querySelector('.dept').style.cssText = 'border-radius: 5px 5px 0 0; margin-top: 10px;';
+    document.getElementById('list').style.display = 'block';
+    document.getElementById('item').value = '';
+    document.getElementById('item').focus();
+  }
+}
 
+//starting script off with initialLoad function
+initialLoad();
 
-
-//populate array with entered item then write list to page
+//event to populate array with entered item then write list to page
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   var item = encodeItem(document.getElementById('item').value);
@@ -84,11 +95,11 @@ form.addEventListener('submit', function(e) {
   var position = shoppingList.findIndex(i => i.dept.toLowerCase() === department.toLowerCase());
   shoppingList[position].items.push(item);                    
   buildList();
+  updateStorage();
   document.querySelector('.dept').style.cssText = 'border-radius: 5px 5px 0 0; margin-top: 10px;';
   document.getElementById('list').style.display = 'block';
   document.getElementById('item').value = '';
   document.getElementById('item').focus();
-  updateStorage();
 });
 
 //sets focus back on the item field after changing department
@@ -102,10 +113,13 @@ document.getElementById('list').addEventListener('click', function(e) {
 	var parent = e.target.parentNode;
 	var depart = parent.previousSibling;
 	var itemToDelete = encodeItem(e.target.innerText);
+	var storageKey = depart.innerText;
+	var storageItems = window.localStorage.getItem(storageKey);
 	for (var i=0; i < shoppingList.length; i++) {
 		if ( depart.innerText == shoppingList[i].dept ) {
 				var deletePos = shoppingList[i].items.findIndex(x => x.items === itemToDelete);
 				shoppingList[i].items.splice(deletePos, 1);
+				window.localStorage.setItem(storageKey, storageItems.replace(',"' + itemToDelete + '"', '').replace('"' + itemToDelete + '",', '').replace('"' + itemToDelete + '"', ''));
 		}
 	}
 	parent.removeChild(e.target);
